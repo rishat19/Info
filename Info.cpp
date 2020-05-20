dbb812714d83275a478d15cc424775d77300a6a3
rishat19 <57725670+rishat19@users.noreply.github.com>
2020-05-20 11:59:12 +0300

    Info added

---
+++ Info.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "I love tea." << std::endl;
+    std::cout << "You can find out other new information about me personally." << std::endl;
+    return 0;
+}
+
---
+++ Info.py
@@ -0,0 +1,2 @@
+print("I love tea.")
+print("You can find out other new information about me personally.")
